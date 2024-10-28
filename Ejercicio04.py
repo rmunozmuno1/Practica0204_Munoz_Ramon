c9586dc84479566fe239ca1e9f503e09001bf434
rmunozmuno1 <rmunozmuno1@educacion.navarra.es>
2024-10-28 12:03:34 +0100

    Create Ejercicio04.py

---
+++ Ejercicio04.py
@@ -0,0 +1,5 @@
+### Escribir un programa que pregunte al usuario los números ganadores de la lotería primitiva, los almacene en una lista y los muestre por pantalla ordenados de menor a mayor.
+Ganador_Uno = float(input('¿Cual es el primer numero ganador?'))
+Ganador_Dos = float(input('Cual es el segundo numero ganador?'))
+Ganador_Tres = float(input('¿Cual es el tercer numero ganador?'))
+Lista_Ganadores = [Ganador_Uno, Ganador_Dos, Ganador_Tres]
