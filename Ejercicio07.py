f401577788f22912fcce89b1050019a09ae20be1
rmunozmuno1 <rmunozmuno1@educacion.navarra.es>
2024-11-04 19:14:09 +0100

    Ejercicio

--- Ejercicio07.py
+++ Ejercicio07.py
@@ -1,4 +1,5 @@
-###Escribir un programa que almacene el abecedario en una lista, elimine de la lista las letras que ocupen posiciones múltiplos de 3, y muestre por pantalla la lista resultante.
-Lista = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-print (Lista.pop(1))
-print (Lista.pop(2))
+# Almacenar el abecedario en una lista
+abecedario = list("abcdefghijklmnopqrstuvwxyz")
+abecedario2 = [letra for i, letra in enumerate(abecedario, start=1) if i % 3 != 0]
+print("Lista del abecedario sin posiciones múltiplos de 3:")
+print(abecedario2)
--- Ejercicio12.py
+++ Ejercicio12.py
@@ -1,1 +1,7 @@
 ### Escribir un programa que pregunte por una muestra de números, separados por comas, los guarde en una lista y muestre por pantalla su media y desviación típica.
+muestra = input('Introduce una muestra de números separados por comas:')
+numeros = [float(num) for num in muestra.split(",")]
+media = sum(numeros) / len(numeros)
+desviacion = (sum((x - media) ** 2 for x in numeros) / len(numeros)) ** 0.5
+print(f'La media de la muestra es: {media:.2f}')
+print(f'La desviación típica de la muestra es: {desviacion:.2f}')
