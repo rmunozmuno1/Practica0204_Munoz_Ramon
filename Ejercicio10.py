8c9923c4ef89b29fa25e6d15eeccf5e6a1293c7c
rmunozmuno1 <rmunozmuno1@educacion.navarra.es>
2024-11-04 11:44:22 +0100

    Ejercicio10

---
+++ Ejercicio10.py
@@ -0,0 +1,4 @@
+### Escribir un programa que almacene en una lista los siguientes precios, 50, 75, 46, 22, 80, 65, 8, y muestre por pantalla el menor y el mayor de los precios.
+Precios = ['08' , '75' , '46' , '22' , '80' , '65' , '50']
+print (max(Precios))
+print (min(Precios))
