afffa5b90d4c400f5317d2066c568bd7e57f6143
rmunozmuno1 <rmunozmuno1@educacion.navarra.es>
2024-11-04 11:37:39 +0100

    Ejercicio

--- Ejercicio08.py
+++ Ejercicio08.py
@@ -1,6 +1,6 @@
 ### Escribir un programa que pida al usuario una palabra y muestre por pantalla si es un palíndromo.
 Palabra = input('Escribe una palabra')
-if Palabra == Palabra[::-1]:
-    print ('si')
+if Palabra == Palabra[::-1]: ### el reversed no me funciona esto es lo mismo pero mejor. :)
+    print ('Si es un palíndromo')
 else:
-    print ('no')
+    print ('No es un palíndromo')
