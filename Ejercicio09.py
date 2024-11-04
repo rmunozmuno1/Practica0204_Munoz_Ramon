5ee651b366c1f249398dd8d0fa6c5fe8c5c5ab4c
rmunozmuno1 <rmunozmuno1@educacion.navarra.es>
2024-11-04 11:35:56 +0100

    Ejercicio

---
+++ Ejercicio09.py
@@ -0,0 +1,7 @@
+### Escribir un programa que pida al usuario una palabra y muestre por pantalla el número de veces que contiene cada vocal.
+Palabra = input('Introduzca una palabra')
+print (Palabra.count('a'))
+print (Palabra.count('e'))
+print (Palabra.count('i'))
+print (Palabra.count('o'))
+print (Palabra.count('u'))
